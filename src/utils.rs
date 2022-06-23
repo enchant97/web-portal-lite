1c755178bfa2a29b331fc424d78790c777d61c55
Leo Spratt <enchant97@users.noreply.github.com>
2022-06-23 21:16:44 +0100

    add password hashing utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,14 @@
 use crate::config::{UserConfig, UserConfigAccount};
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
 use rocket::fs::NamedFile;
 use rocket::http::CookieJar;
 use serde_yaml;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter};
 use std::path::{Path, PathBuf};
 
 /// Will try to load icon from svg folder then png.
@@ -33,6 +37,13 @@ pub fn read_user_config(config_path: &PathBuf) -> std::io::Result<UserConfig> {
     Ok(user_config)
 }
 
+pub fn write_user_config(config_path: &PathBuf, config: &UserConfig) {
+    // TODO convert to async using tokio
+    // FIXME get rid of unwrap usage
+    let file = File::create(config_path).unwrap();
+    serde_yaml::to_writer(BufWriter::new(file), config).unwrap();
+}
+
 /// Returns whether user is authenticated and actually exists
 pub fn ensure_authenticated(
     cookies: &CookieJar,
@@ -44,5 +55,33 @@ pub fn ensure_authenticated(
             None => Err(()),
         },
         None => Err(()),
+    }
+}
+
+/// Returns a hashed password using Argon2 into the 'PHC string' format
+pub fn create_hashed_password(password: &str) -> Result<String, ()> {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+    match argon2.hash_password(password.as_bytes(), &salt) {
+        Ok(hash) => Ok(hash.to_string()),
+        Err(_) => Err(()),
+    }
+}
+
+/// Verifies hashed password in the 'PHC string' format with a plain text one
+pub fn verify_hashed_password(password: &str, password_hash: &str) -> Result<bool, ()> {
+    match PasswordHash::new(password_hash) {
+        Ok(hash) => Ok(Argon2::default()
+            .verify_password(password.as_bytes(), &hash)
+            .is_ok()),
+        Err(_) => Err(()),
+    }
+}
+
+/// Check whether password has been hashed
+pub fn is_hashed_password(possible_hash: &str) -> bool {
+    match PasswordHash::new(possible_hash) {
+        Ok(_) => true,
+        Err(_) => false,
     }
 }
