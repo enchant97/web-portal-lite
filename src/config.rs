9e9227a8df98046a17077afa527fe4bc28a10e8f
Leo Spratt <enchant97@users.noreply.github.com>
2022-06-23 15:37:03 +0100

    add user config

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
-use rocket::serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 #[derive(Debug, Deserialize)]
@@ -6,4 +7,31 @@ pub struct ServerConfig {
     pub config_path: PathBuf,
     pub icons_path: PathBuf,
     pub guest_allowed: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UserConfigLink {
+    title: String,
+    color_name: Option<String>,
+    icon_name: Option<String>,
+    href: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UserConfigDashboard {
+    title: String,
+    show_header: bool,
+    links: Vec<UserConfigLink>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UserConfigAccount {
+    password: String,
+    dashboard: Option<Vec<UserConfigDashboard>>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UserConfig {
+    config_version: usize,
+    accounts: HashMap<String, UserConfigAccount>,
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,18 @@
+use crate::config::UserConfig;
 use rocket::fs::NamedFile;
+use serde_yaml;
+use std::fs::File;
+use std::io::BufReader;
 use std::path::{Path, PathBuf};
 
 /// Will try to load icon from svg folder then png.
 pub async fn load_named_icon(icon_name: &str, icons_path: &PathBuf) -> Option<NamedFile> {
-    let svg_path = Path::new(icons_path).join("svg").join(String::from(icon_name) + ".svg");
-    let png_path = Path::new(icons_path).join("png").join(String::from(icon_name) + ".png");
+    let svg_path = Path::new(icons_path)
+        .join("svg")
+        .join(String::from(icon_name) + ".svg");
+    let png_path = Path::new(icons_path)
+        .join("png")
+        .join(String::from(icon_name) + ".png");
 
     let svg_file = NamedFile::open(svg_path).await.ok();
 
@@ -12,4 +20,13 @@ pub async fn load_named_icon(icon_name: &str, icons_path: &PathBuf) -> Option<Na
         Some(_) => svg_file,
         None => NamedFile::open(png_path).await.ok(),
     }
+}
+
+/// Read a user config file and return it
+pub fn read_user_config(config_path: &PathBuf) -> std::io::Result<UserConfig> {
+    // TODO convert to async using tokio
+    // FIXME get rid of unwrap usage
+    let file = File::open(config_path)?;
+    let user_config: UserConfig = serde_yaml::from_reader(BufReader::new(file)).unwrap();
+    Ok(user_config)
 }
