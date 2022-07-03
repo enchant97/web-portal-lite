57190f2bb2ce012b5ced454c1eb7a97e16593402
Leo Spratt <enchant97@users.noreply.github.com>
2022-07-03 22:41:16 +0100

    add js theme changer

---
+++ web-portal-lite/static/script.js
@@ -0,0 +1,9 @@
+ThemeChanger.theme_picker_parent = document.querySelector("main");
+ThemeChanger.use_local = true;
+ThemeChanger.selected_theme_css_class = "green";
+
+let themeToggleBnt = document.getElementById("themeToggleBnt")
+themeToggleBnt.addEventListener("click", ThemeChanger.toggle_theme_picker);
+themeToggleBnt.classList.remove("hidden");
+
+ThemeChanger.on_load();
